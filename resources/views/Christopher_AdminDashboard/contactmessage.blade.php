e0a0f51dc577eeb6e38b94a89393909cdaf61af4
Thet-Htar-Thu-Zar <thethtarthuzar648@gmail.com>
2025-02-09 21:06:25 +0630

    -

--- resources/views/Christopher_AdminDashboard/contactmessage.blade.php
+++ resources/views/Christopher_AdminDashboard/contactmessage.blade.php
@@ -3,24 +3,25 @@
 
 <table class="table table-striped table-hover mt-3" style="border-spacing: 0 10px;">
     <thead>
-      <tr>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Message</th>
-        <th>Remove</th>
-      </tr>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Message</th>
+            <th>Remove</th>
+        </tr>
     </thead>
 
     <tbody>
         @foreach($messages as $msg)
-        <tr>
-            <td>{{$msg->name}}</td>
-            <td>{{$msg->email}}</td>
-            <td>{{$msg->message}}</td>
-            <td class="text-center">
-                <a href="{{route('deletemessage', $msg->msg_id)}}" class="btn btn-fantastic"><i class="fas fa-trash-alt"></i></a>
-            </td>
-        </tr>
+            <tr>
+                <td>{{$msg->name}}</td>
+                <td>{{$msg->email}}</td>
+                <td>{{$msg->message}}</td>
+                <td class="text-center">
+                    <a href="{{route('deletemessage', $msg->msg_id)}}" class="btn btn-fantastic"><i
+                            class="fas fa-trash-alt"></i></a>
+                </td>
+            </tr>
         @endforeach
     </tbody>
 </table>
@@ -30,19 +31,24 @@
         border-collapse: separate;
         border-spacing: 0 10px;
     }
+
     .table th {
         background-color: #3494be;
         color: white;
         text-transform: uppercase;
     }
+
     .table-striped tbody tr:nth-of-type(odd) {
         background-color: #F0F8FF;
     }
+
     .table-hover tbody tr:hover {
         background-color: #E6E6FA;
         transition: background-color 0.3s ease-in-out;
     }
-    .table td, .table th {
+
+    .table td,
+    .table th {
         padding: 15px;
         vertical-align: middle;
         border-top: 2px solid #ffffff;
@@ -56,12 +62,12 @@
         transition: all 0.3s ease-in-out;
         padding: 5px 20px;
     }
+
     .btn-fantastic:hover {
         background-color: #FF6347;
         box-shadow: 0px 6px 8px rgba(0, 0, 0, 0.2);
         transform: scale(1.05);
     }
 </style>
-
 
 @endsection
--- resources/views/Christopher_AdminDashboard/login.blade.php
+++ resources/views/Christopher_AdminDashboard/login.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,57 +8,60 @@
     <title>Christopher_AdminDashboard</title>
     <link rel="stylesheet" href="https://cdn.bootcss.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" >
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/styleadmin.css') }}">
 </head>
+
 <body>
 
-  @if($errors->any())
-<div class="alert alert-warning text-danger">
-  <ul>
-    @foreach($errors->all() as $error)
-    <li>{{$error}}</li>
-    @endforeach
-  </ul>
-  </div>
-@endif
-<form action="{{route('adminLogin')}}" method="post">
-    @csrf
-  <div class="container-scroller">
-    <div class="container-fluid page-body-wrapper full-page-wrapper">
-      <div class="row w-100 m-0">
-        <div class="content-wrapper full-page-wrapper d-flex align-items-center auth login-bg">
-          <div class="card col-lg-4 mx-auto">
-            <div class="card-body px-5 py-5">
-              <h3 class="card-title text-left mb-3">Login</h3>
-              <form>
-                <div class="form-group">
-                  <label>Email</label>
-                  <input type="text" class="form-control p_input" name="email" style="color: white">
-                </div>
-                <div class="form-group">
-                  <label>Password</label>
-                  <input type="password" class="form-control p_input" name="password" style="color: white">
-                </div>
-
-                <div class="text-center">
-                  <button type="submit" class="btn btn-block enter-btn" style="background-color: black">Login</button>
-                  <button type="reset" class="btn btn-danger btn-block enter-btn">Cancel</button>
-                </div>
-
-              </form>
-            </div>
-          </div>
+    @if($errors->any())
+        <div class="alert alert-warning text-danger">
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
         </div>
-      </div>
-    </div>
-  </div>
-</form>
-  <script src="js/vendor.bundle.base.js"></script>
-  <script src="js/off-canvas.js"></script>
-  <script src="js/hoverable-collapse.js"></script>
-  <script src="js/misc.js"></script>
-  <script src="js/settings.js"></script>
-  <script src="js/todolist.js"></script>
+    @endif
+    <form action="{{route('adminLogin')}}" method="post">
+        @csrf
+        <div class="container-scroller">
+            <div class="container-fluid page-body-wrapper full-page-wrapper">
+                <div class="row w-100 m-0">
+                    <div class="content-wrapper full-page-wrapper d-flex align-items-center auth login-bg">
+                        <div class="card col-lg-4 mx-auto">
+                            <div class="card-body px-5 py-5">
+                                <h3 class="card-title text-left mb-3">Login</h3>
+                                <form>
+                                    <div class="form-group">
+                                        <label>Email</label>
+                                        <input type="text" class="form-control p_input" name="email"
+                                            style="color: white">
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Password</label>
+                                        <input type="password" class="form-control p_input" name="password"
+                                            style="color: white">
+                                    </div>
 
+                                    <div class="text-center">
+                                        <button type="submit" class="btn btn-block enter-btn"
+                                            style="background-color: black">Login</button>
+                                        <button type="reset" class="btn btn-danger btn-block enter-btn">Cancel</button>
+                                    </div>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+    <script src="js/vendor.bundle.base.js"></script>
+    <script src="js/off-canvas.js"></script>
+    <script src="js/hoverable-collapse.js"></script>
+    <script src="js/misc.js"></script>
+    <script src="js/settings.js"></script>
+    <script src="js/todolist.js"></script>
 </body>
